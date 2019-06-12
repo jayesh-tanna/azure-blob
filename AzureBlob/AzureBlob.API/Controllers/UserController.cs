42006765958d1c5b5846f54b2ab9657269a5719b
Jayesh Tanna <tanna.jay90@gmail.com>
2019-06-12 15:59:00 +0530

    user can upload profile pic to azure blob.

---
+++ AzureBlob/AzureBlob.API/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace AzureBlob.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("SaveProfilePic")]
+        public async Task<IActionResult> SaveProfilePicAsync(IFormFile file)
+        {
+            var storageConnectionString = _configuration["ConnectionStrings:AzureStorageConnectionString"];
+
+            if (CloudStorageAccount.TryParse(storageConnectionString, out CloudStorageAccount storageAccount))
+            {
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+                CloudBlobContainer container = blobClient.GetContainerReference("profilepic");
+
+                await container.CreateIfNotExistsAsync();
+
+                //MS: Don't rely on or trust the FileName property without validation. The FileName property should only be used for display purposes.
+                var picBlob = container.GetBlockBlobReference(file.FileName);
+
+                await picBlob.UploadFromStreamAsync(file.OpenReadStream());
+
+                return Ok(picBlob.Uri);
+            }
+
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+}
--- AzureBlob/AzureBlob.API/Startup.cs
+++ AzureBlob/AzureBlob.API/Startup.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace AzureBlob.API
 {
@@ -33,6 +27,10 @@ namespace AzureBlob.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler();
             }
 
             app.UseMvc();
